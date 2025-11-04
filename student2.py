8bb78a26526b6d1ca724aa99a1c2389053a4a110
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-04 11:44:17 +0530

    Fix typo in variable name for script_name

--- student2.py
+++ student2.py
@@ -1,6 +1,6 @@
 import sys
 if len(sys.argv) == 3:
-    scipt_name=sys.argv[0]
+    script_name=sys.argv[0]
     name=sys.argv[1]
     rollno=sys.argv[2]
     print("user provided input values:")
@@ -10,7 +10,7 @@ else:
     name="Nagaraj"
     rollno="101"
     print("no input given - using default values:")
-print("Scipt Name:",scipt_name)
+print("Scipt Name:",script_name)
 print("Student Name:",name)
 print("Roll no:",rollno)
 
