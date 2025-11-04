0ae7784dd119fc7a3f25a2764eb12eefc2fd9357
Nagaraj Kuntoji <kn733478@gmail.com>
2025-11-04 11:39:36 +0530

    Update student2.py

--- student2.py
+++ student2.py
@@ -1,22 +1,16 @@
-name="Raju"
-rollno="123"
-print("Student Name:",name)
-print("Student Roll no:",rollno)
-
-
 import sys
 if len(sys.argv) == 3:
     scipt_name=sys.argv[0]
     name=sys.argv[1]
-    roll_no=sys.argv[2]
+    rollno=sys.argv[2]
     print("user provided input values:")
 
 else:
     script name=sys.argv[0]
     name="Nagaraj"
-    roll_no="101"
+    rollno="101"
     print("no input given -using default values:")
 print("Scipt Name:",scipt_name)
 print("Student Name:",name)
-print("Roll no:",roll_no)
+print("Roll no:",rollno)
 
